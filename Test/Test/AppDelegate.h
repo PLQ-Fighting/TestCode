096b89e36773a9303d99c037c112b30a1b0d315d
PLQ <peiliqiao@peiliqiaoMacBook-Pro.local>
2019-04-18 11:08:36 +0800

    Test

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by PLQ on 2019/4/18.
+//  Copyright © 2019年 PLQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by PLQ on 2019/4/18.
+//  Copyright © 2019年 PLQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
